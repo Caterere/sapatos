e1df7f1719d58c91d73d7bb37c784bcb33c2b1c5
LUIZ EDUARDO ORO DE SOUZA <luiz_eo_souza@estudante.sesisenai.org.br>
2023-05-22 09:42:06 -0300

    sapatos manutenção

---
+++ assets/js/style.js
@@ -0,0 +1,8 @@
+let buttonativamenu = document.querySelector('.buttonativamenu')
+let navbar = document.querySelector('.navbar')
+
+
+buttonativamenu.addEventListener('click', ()=>{
+    navbar.classList.toggle('navbarativo')
+})
+
